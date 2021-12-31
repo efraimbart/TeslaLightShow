1f36500e2b75599ef64a962d3ec4fc161be2ea0f
efraimbart <efraim@rallybound.com>
2021-12-31 16:22:06 -0500

    Attempt to update theme color

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,8 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
+      lang: 'en',
+      theme_color: '#43151f'
     }
   },
 
