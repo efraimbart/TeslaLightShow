18b33b37a8784375f126c289ac1ea3d12ece8bf7
efraimbart <efraim@rallybound.com>
2022-01-06 00:45:34 -0500

    Remove newer incompatible syntax

--- common/util.js
+++ common/util.js
@@ -4,7 +4,7 @@ const indexlessArraysPattern = /[^&]*(\[][^=]*)/g
 export const replaceIndexlessArrays = (str) => {
   const matchSets = {}
   return str.replace(indexlessArraysPattern, (match, p1) => {
-    matchSets[match] ||= 0
+    matchSets[match] = matchSets[match] || 0
     const matchCount = matchSets[match]++
     const p1Replaced = p1.replace('[]', `[${matchCount}]`)
     return match.replace(p1, p1Replaced)
